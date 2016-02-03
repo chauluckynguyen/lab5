4a6a852d2d7b68200867020bf1cd07921ec6f400
LuckyNguyen <lkn007@ucsd.edu>
2016-02-03 03:34:01 +0000

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,32 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+
+exports.addFriend = function(req, res) { 
+
+	var newFriend = {
+			"name": req.query.name,
+			"description": req.query.description,
+			"imageURL": "http://lorempixel.com/400/400/people"
+	};
+
+	console.log(newFriend);
+
+	data["friends"].push(newFriend);
+	res.render('add', {allmyfriends: data["friends"]});
+
+};
+
+exports.view = function(req, res){
+  res.render('add', {
+  	'allmyfriends':[
+	{'name': 'Grace Hopper',
+     'image': 'http://lorempixel.com/500/500/people',
+     'description': 'my friend is named A'
+ 	},
+ 	{'name': 'Doug Engelbart',
+     'image': 'http://lorempixel.com/500/500/people',
+     'description': 'myfriend is named B'
+ 	}
+ 	]
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,21 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', {allmyfriends:data["friends"]});
 };
+
+/*
+exports.view = function(req, res){
+  res.render('index', {
+  	'allmyfriends':[
+	{'name': 'Grace Hopper',
+     'image': 'http://lorempixel.com/500/500/people',
+     'description': 'my friend is named A'
+ 	},
+ 	{'name': 'Doug Engelbart',
+     'image': 'http://lorempixel.com/500/500/people',
+     'description': 'myfriend is named B'
+ 	}
+ 	]
+    });
+};*/
